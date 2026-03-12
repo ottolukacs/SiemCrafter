13f3dd9073be7aa18af99cdef522fbf735056aa1
Otto Lukacs <ottolukacs@ottolukacs.com>
2026-03-12 13:39:38 +0100

    Update jumpstart script and inventory configuration for consistent setup directory

--- iac/jumpstart.sh
+++ iac/jumpstart.sh
@@ -12,7 +12,7 @@ add-apt-repository --yes --update ppa:ansible/ansible
 apt install ansible -y
 
 # Clone necessary Git Repos
-git clone https://github.com/ottolukacs/Siemcrafter.git {{ setup_directory }}
+git clone https://github.com/ottolukacs/Siemcrafter.git /home/ottolukacs/siemcrafter
 
 # Copy roles to ansible
 # cp -r /home/ubuntu/blocks-lab/cyberlab-deployer/roles/blocks-server /etc/ansible/roles/blocks-server
